57727e88f86037212fa7be3a466be2ebcd3cdfd1
Maxaz <max.nazarski@gmail.com>
2023-05-19 11:51:23 +0200

    Added the Publisher model

---
+++ Section2/spring-6-webapp/src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,69 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author author1 = new Author();
+        author1.setFirstName("Neil");
+        author1.setLastName("Gaiman");
+
+        Book book1 = new Book();
+        book1.setTitle("American Gods");
+        book1.setIsbn("1234567");
+
+        Author author1Saved = authorRepository.save(author1);
+        Book book1Saved = bookRepository.save(book1);
+
+        Author author2 = new Author();
+        author2.setFirstName("Frank");
+        author2.setLastName("Herbert");
+
+        Book book2 = new Book();
+        book2.setTitle("Dune");
+        book2.setIsbn("7654321");
+
+        Author author2Saved = authorRepository.save(author2);
+        Book book2Saved = bookRepository.save(book2);
+
+        author1Saved.getBooks().add(book1Saved);
+        author2Saved.getBooks().add(book2Saved);
+
+        Publisher publisher1 = new Publisher();
+        publisher1.setPublisherName("qwerty");
+        Publisher publisher1Saved = publisherRepository.save(publisher1);
+
+        publisher1Saved.getBooks().add(book1Saved);
+        publisher1Saved.getBooks().add(book2Saved);
+
+        authorRepository.save(author1Saved);
+        authorRepository.save(author2Saved);
+        publisherRepository.save(publisher1Saved);
+
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author count: " + authorRepository.count());
+        System.out.println("Book count: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
+    }
+}
--- Section2/spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ Section2/spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
+
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -14,7 +16,7 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Set<Book> getBooks() {
         return books;
