ad9f15ee3fea677a271d101bcf7c2d46657aaeb7
Maxaz <max.nazarski@gmail.com>
2023-05-22 13:09:58 +0200

    Init Section3

---
+++ Section3/spring-6-section3/src/test/java/guru/springframework/spring6section3/Spring6Section3ApplicationTests.java
@@ -0,0 +1,34 @@
+package guru.springframework.spring6section3;
+
+import guru.springframework.spring6section3.controllers.MyController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class Spring6Section3ApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowiredOfController() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
