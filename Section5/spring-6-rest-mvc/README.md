2885e354f28796b7d33431404a80862ca9a46306
Maxaz <max.nazarski@gmail.com>
2023-05-26 12:42:24 +0200

    init HTTP GET with Spring MVC of the beer list

--- Section5/spring-6-rest-mvc/README.md
+++ Section5/spring-6-rest-mvc/README.md
@@ -2,4 +2,4 @@
 
 ---
 Spring Framework 6: Beginner to Guru by John Thompson  
-Section 5 of the course : Project Lombok
+Section 5 & 6 of the course : Project Lombok & Spring MVC Rest Services
--- Section5/spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ Section5/spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -4,10 +4,13 @@ import guru.springframework.spring6restmvc.model.Beer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }
